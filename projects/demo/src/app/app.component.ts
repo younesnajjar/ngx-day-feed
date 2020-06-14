c26df9f46106fa1392154ff7d48c299bf71d6965
Younes Najjar <younes.najjar.96@gmail.com>
2020-06-14 20:52:35 +0100

    Disactivate filter
    # This feature is not ready yet

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -43,7 +43,7 @@ export class AppComponent {
   };
 
   showMessage($event: any) {
-    this.data = this.data.filter((item, index) => index !== $event.index);
+    // this.data = this.data.filter((item, index) => index !== $event.index);
     console.log(this.data);
   }
 }
